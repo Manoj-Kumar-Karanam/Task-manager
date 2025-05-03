e70ae47e71656d6865e16439230efb09262f4050
Manoj <manojkumarkaranam2005@gmail.com>
2025-05-03 22:27:00 +0530

    user model completed

--- models/user.go
+++ models/user.go
@@ -1,6 +1,12 @@
 package models
 
 import (
-	"fmt"
 	"gorm.io/gorm"
 )
+type User struct {
+	gorm.Model
+	// ID uint `gorm:"primarykey"` //
+	Username string `gorm:"unique; not null"`
+	Email string	`gorm:"unique; not null"`
+	Password string	`gorm:"not null"`
+}
