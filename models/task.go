a860ea5ba25731b66964fe2feead2a1243e6ea89
Manoj <manojkumarkaranam2005@gmail.com>
2025-05-03 22:28:27 +0530

    added task model

---
+++ models/task.go
@@ -0,0 +1,12 @@
+package models
+
+import "gorm.io/gorm"
+
+type Task struct {
+	gorm.Model
+	Title       string `gorm:"not null"`
+	Description string
+	Completed   bool   `gorm:"default:false"`
+	UserID      uint   // foreign key, references User table
+	User        User   `gorm:"foreignKey:UserID"`
+}
