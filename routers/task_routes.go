010432a41d678162ff83fe347ea93f083595f279
Manoj <manojkumarkaranam2005@gmail.com>
2025-05-31 11:54:18 +0530

    updated the taskroutes with authentication middleware

--- routers/task_routes.go
+++ routers/task_routes.go
@@ -2,18 +2,22 @@ package routers
 
 import (
 	"github.com/gin-gonic/gin"
+	"task_manager/middleware"
 	"task_manager/controllers"
 )
 
-func TaskRoutes(r *gin.Engine) {
-	taskGroup := r.Group("/tasks") 
-	{
-		taskGroup.GET("/", controllers.GetallTasks)
-		taskGroup.GET("/:id", controllers.GetTaskbyId)
-		taskGroup.POST("/", controllers.CreateTask)
-		taskGroup.PUT("/:id", controllers.UpdateTask)
-		taskGroup.DELETE("/:id", controllers.DeleteTask)
-	}
+func TaskRoutes(router *gin.Engine) {
 	
-}
+	router.GET("/tasks", controllers.GetallTasks)
 
+	// Authenticated user routes
+	auth := router.Group("/api")
+	auth.Use(middleware.AuthMiddleware()) // This middleware sets userID into context
+	{
+		auth.POST("/tasks", controllers.CreateTask)         // Create a task
+		auth.GET("/tasks", controllers.GetUsertasks)        // List only the user's tasks
+		auth.GET("/tasks/:id", controllers.GetTaskbyId)     // Get one task (with ownership check)
+		auth.PUT("/tasks/:id", controllers.UpdateTask)      // Update task (user only)
+		auth.DELETE("/tasks/:id", controllers.DeleteTask)   // Delete task (user only)
+	}
+}
