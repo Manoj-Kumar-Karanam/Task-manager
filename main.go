71dd99f23330d417553a48d3dfb08430248fd1c0
Manoj <manojkumarkaranam2005@gmail.com>
2025-05-31 12:10:54 +0530

    a working main.go file

--- main.go
+++ main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	// "fmt"
+	// "fmt"
 	"log"
 	"task_manager/config"
+	// "task_manager/controllers"
 	"task_manager/models"
+	"task_manager/routers"
 
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
@@ -13,6 +18,12 @@ func main() {
     if err != nil {
         log.Fatal("Failed to auto migrate models:", err)
     }
-
+	// fmt.Println("out")
+	// config.DB.AutoMigrate(&models.User{}, &models.Task{})
+	// fmt.Print(controllers.GetUserProfile(&gin.Context{}))
+	r := gin.Default()
+	routers.TaskRoutes(r)
+	routers.UserRoutes(r)
+	r.Run(":8080")
 }
 
